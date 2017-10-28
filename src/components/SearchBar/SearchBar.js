90b0966d728148a7d519e82b77bc5c27ada3c9d7
Generosa Litton <generosa.litton@yahoo.com>
2017-10-28 15:18:00 -0700

    added bind

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -60,7 +60,7 @@ class SearchBar extends React.Component {
       Object.keys(this.sortByOptions).map(sortByOption => {
         let sortByOptionValue = this.sortByOptions[sortByOption];
         return (
-          <li onClick={this.handleSortByChange} className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue}>{sortByOption}</li>
+          <li onClick={this.handleSortByChange.bind(this, sortByOptionValue)} className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue}>{sortByOption}</li>
         );  
       });
   }
